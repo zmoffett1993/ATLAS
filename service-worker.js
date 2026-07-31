5adacf9a7a2487187adf486f1f14d1c96e68780c
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-07-31 07:39:39 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v31-inventory-aware-ocr";
+const VERSION = "atlas-pwa-v33-emergency-logo-and-menu-fix";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
