52e83b4ca2092f777df15c6c81252c9636a31151
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-07-31 23:14:05 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v45-driveway-labels-102f2";
+const VERSION = "atlas-pwa-v46-sku-hierarchy-102f3";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
