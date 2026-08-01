27e9b135b8a3454ed449cf6f88aaa013ba17bc1a
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-01 11:38:03 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v48-premium-navigation-11";
+const VERSION = "atlas-pwa-v49-aisle1-sku-focus-111";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
