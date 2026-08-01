b4cd7608a3717281b375389667253eb843174fdd
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-07-31 20:06:03 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v36-create-sku-raw-input";
+const VERSION = "atlas-pwa-v37-production-polish-cleanup";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
