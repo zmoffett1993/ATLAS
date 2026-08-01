dacb705e6609dc7c27751c3b4c3d6b0fa4a29437
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-01 11:01:29 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v46-sku-hierarchy-102f3";
+const VERSION = "atlas-pwa-v47-picker-first-product-103";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
