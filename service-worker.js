14494b76fdf111a9c6e1039a593bd2b10685db1d
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-05 06:38:02 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v103-glove-ready-command-dock";
+const VERSION = "atlas-pwa-v104-focused-scan-sku-camera";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
