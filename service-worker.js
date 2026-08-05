9ae0b654768b03dca163879e84f25423061b854f
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-04 22:13:27 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v98-android-voice-toggle";
+const VERSION = "atlas-pwa-v99-smart-ocr-search";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
