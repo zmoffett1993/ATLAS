acaac5d744b961e2347c147958edffcae45fd49a
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-05 20:48:50 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v107-android-launch-continuity";
+const VERSION = "atlas-pwa-v110-pallet-spreadsheet-authority";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
@@ -7,6 +7,7 @@ const APP_SHELL = [
   "./index.html",
   "./manifest.webmanifest?v=107",
   "./product-images.json",
+  "./pallet-guides.json?v=20260805-v3",
   "./product-images/cgasb1-60ml-cnl-owh.png",
   "./atlas-icon-v2-180.png?v=101",
   "./atlas-icon-v2-192.png?v=101",
