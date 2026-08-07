6adbe1610b5b62fd66dded229e2f31ea765175f2
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-06 18:03:29 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v112-results-focus-map-disclosure";
+const VERSION = "atlas-pwa-v113-inventory-consolidation-back-button";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
