d565f6203ce2d15b01a9343733cf9c4472cf305f
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-06 18:21:46 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v114-compact-inventory-back-button";
+const VERSION = "atlas-pwa-v115-inventory-menu-order";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
