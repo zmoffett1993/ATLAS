3c240e5ca34858d441414a1e7c9734751ef3b97a
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-16 20:54:22 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v146-vibrant-alerts";
+const VERSION = "atlas-pwa-v147-dosing-cup-framed";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
@@ -15,7 +15,7 @@ const APP_SHELL = [
   "./atlas-alerts.css?v=1",
   "./atlas-desktop.js?v=128",
   "./manifest.webmanifest?v=107",
-  "./product-images.json?v=20260816-new-sku-images-103",
+  "./product-images.json?v=20260816-dosing-cup-framed-v104",
   "./pallet-guides.json?v=20260817-new-skus-v6",
   "./product-images/cgasb1-60ml-cnl-owh.png",
   "./atlas-icon-v2-180.png?v=101",
