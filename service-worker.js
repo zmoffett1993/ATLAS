10afa115db0169a4ad9dc04a7432ab9bb29a8c57
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-20 21:05:09 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v174-location-actions-freeze-fix";
+const VERSION = "atlas-pwa-v170-desktop-scaled-map";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
@@ -15,9 +15,7 @@ const APP_SHELL = [
   "./atlas-desktop.css?v=143",
   "./atlas-desktop-menu-typography.css?v=1",
   "./atlas-alerts.css?v=2",
-  "./atlas-desktop.js?v=132",
-  "./atlas-location-actions.css?v=2",
-  "./atlas-location-actions.js?v=2",
+  "./atlas-desktop.js?v=130",
   "./manifest.webmanifest?v=107",
   "./product-images.json?v=20260816-dosing-cup-framed-v104",
   "./pallet-guides.json?v=20260817-new-skus-v6",
