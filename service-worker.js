a8af2e0912b8012fc7413ee44ce10372dd231114
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-29 23:38:54 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v206-premium-coc-receiver";
+const VERSION = "atlas-pwa-v207-centered-coc-customer";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
@@ -9,7 +9,7 @@ const APP_SHELL = [
   "./atlas-dashboard.js?v=148",
   "./atlas-auth.css?v=2",
   "./atlas-auth.js?v=3",
-  "./atlas-coc.css?v=20",
+  "./atlas-coc.css?v=21",
   "./atlas-zxing-browser.min.js?v=1",
   "./atlas-coc-scanner.js?v=7",
   "./atlas-coc-parser.js?v=7",
