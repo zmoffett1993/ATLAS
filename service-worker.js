5395eb60513ccf6b175c9edc156a0d51ab5e7b3c
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-08-30 20:22:00 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v241-native-lot-caret-editing";
+const VERSION = "atlas-pwa-v242-receiver-navigation-buttons";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
@@ -21,8 +21,8 @@ const APP_SHELL = [
   "./atlas-coc-excel.js?v=16",
   "./atlas-coc.js?v=47",
   "./coc-receiver/index.html",
-  "./coc-receiver/receiver.css?v=14",
-  "./coc-receiver/receiver.js?v=13",
+  "./coc-receiver/receiver.css?v=15",
+  "./coc-receiver/receiver.js?v=14",
   "./atlas-guided-workflows.css?v=4",
   "./atlas-guided-workflows.js?v=4",
   "./atlas-restock.css?v=3",
