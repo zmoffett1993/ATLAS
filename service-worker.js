026443f4a9fb51fedc516fc3766a71381924b60c
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-09-10 23:31:55 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v313-receiver-completion-hold";
+const VERSION = "atlas-pwa-v314-receiver-completion-centered";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
@@ -23,7 +23,7 @@ const APP_SHELL = [
   "./atlas-coc.js?v=80",
   "./coc-receiver/index.html",
   "./coc-receiver/receiver.css?v=34",
-  "./coc-receiver/receiver.js?v=35",
+  "./coc-receiver/receiver.js?v=36",
   "./atlas-guided-workflows.css?v=4",
   "./atlas-guided-workflows.js?v=4",
   "./atlas-restock.css?v=3",
