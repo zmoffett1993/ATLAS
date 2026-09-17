47fac51b8fce372b2f722a1bd0ce04390b68f793
zmoffett1993 <zachary.c.moffett@gmail.com>
2026-09-17 06:21:34 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = "atlas-pwa-v315-sidebar-toggle-refinement";
+const VERSION = "atlas-pwa-v316-attached-sidebar-toggle";
 const SHELL_CACHE = `${VERSION}-shell`;
 const DATA_CACHE = `${VERSION}-warehouse-data`;
 
@@ -29,7 +29,7 @@ const APP_SHELL = [
   "./atlas-restock.css?v=3",
   "./atlas-restock.js?v=3",
   "./atlas-delete-approval.js?v=1",
-  "./atlas-desktop.css?v=148",
+  "./atlas-desktop.css?v=149",
   "./atlas-desktop-menu-typography.css?v=1",
   "./atlas-alerts.css?v=2",
   "./atlas-mobile-menu.css?v=2",
